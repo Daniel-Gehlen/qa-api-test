ee94c470b4f086d159f5ea3cb046fadb18b64598
Daniel Gehlen <142283217+Daniel-Gehlen@users.noreply.github.com>
2024-03-15 07:47:38 -0300

    Update loginUser.spec.js

--- loginUser.spec.js
+++ loginUser.spec.js
@@ -32,3 +32,22 @@ describe('API - Login de Usuário', () => {
     })
   });
 });
+
+
+describe('Web - Login de Usuário', () => {
+  it('Deve fazer login com um usuário válido', () => {
+    cy.visit('/login');
+    cy.get('[data-cy=email]').type('example@example.com');
+    cy.get('[data-cy=password]').type('examplePassword');
+    cy.get('[data-cy=login-button]').click();
+    cy.url().should('include', '/home');
+  });
+
+  it('Não deve fazer login com credenciais inválidas', () => {
+    cy.visit('/login');
+    cy.get('[data-cy=email]').type('example@example.com');
+    cy.get('[data-cy=password]').type('wrongPassword');
+    cy.get('[data-cy=login-button]').click();
+    cy.contains('email or password is invalid');
+  });
+});
